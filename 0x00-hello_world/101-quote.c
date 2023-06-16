f19ea29611c7f47e262df65f4306749dfa5f4efd
G-omar-H <omarghazi12@gmail.com>
2023-06-16 04:16:53 -0700

    0x00C

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
  * Description: print a quote using write function
- *	write(int fd.const void *buff.size_t count)
+ *	write(int fd.const void *buf.size_t count)
  * Return: 1 (not success)
 */
 int main(void)
 {	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(1.quo.59);
+	write(2, quo, 59);
 	return (1);
 }
