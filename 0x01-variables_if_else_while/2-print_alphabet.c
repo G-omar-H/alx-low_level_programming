c4d572957a2a3f1a14bf119c395e6a70d5d63bc0
G-omar-H <omarghazi12@gmail.com>
2023-06-17 11:50:09 -0700

    0x01C

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: C programme that prints alphabets in lowercase
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		putchar(alphabet);
+	}
+	putchar('\n');
+	return (0);
+}
