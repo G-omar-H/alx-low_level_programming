8194a4e10162c899449aa45212ed4d6826ff707b
G-omar-H <omarghazi12@gmail.com>
2023-06-21 12:10:17 -0700

    0x02C

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -2,9 +2,10 @@
 /**
  * main - Entry point
  * Description: C programme that prints all numbers
- * 	from 0 to 1024 that are
- * 	multiplication of 3 or 5 and return the sum
- * Return: Always 0 (success)*/
+ *	from 0 to 1024 that are
+ *	multiplication of 3 or 5 and return the sum
+ * Return: Always 0 (success)
+*/
 int main(void)
 {
 	int num, sum;
@@ -17,4 +18,3 @@ int main(void)
 	printf("%d\n", sum);
 	return (0);
 }
-		
