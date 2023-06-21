e0dff03d55933a0ec53f0b4f837116b8bde96e8b
G-omar-H <omarghazi12@gmail.com>
2023-06-21 00:59:36 -0700

    0x02C

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,9 +6,9 @@ void jack_bauer(void)
 {
 	int hr, min;
 
-	for(hr = 0; hr <= 23; hr++)
+	for (hr = 0; hr <= 23; hr++)
 	{
-		for(min = 0; min <= 59; min++)
+		for (min = 0; min <= 59; min++)
 		{
 			_putchar((hr / 10) + 48);
 			_putchar((hr % 10) + 48);
