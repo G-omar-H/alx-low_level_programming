2323bae00f2c55bb08b55bcd87a6324115e4fe96
G-omar-H <omarghazi12@gmail.com>
2023-06-21 00:16:40 -0700

    0x02C

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - introduce abs function
+ * @c: takes in integer type function
+ * Description: function that computize absolute value of an integral
+ * Return: absolute value of int
+*/
+int _abs(int c)
+{
+	if (c < 0)
+		c = (-1) * c;
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,11 +2,12 @@
 #define MAIN_H
 
 #include <stdio.h>
-
+#include <stdlib.h>
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
+int _abs(int);
 #endif
