14206deae0944f9f7e62c6d9745e5c196e1d684b
G-omar-H <omarghazi12@gmail.com>
2023-06-22 14:05:59 -0700

    0x03C

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * more_numbers - print numbers from 0 to 14, 10 times
+ * Return: 0
+ */
+void more_numbers(void)
+{
+	int num, line, count;
+
+	for (line = 1; line <= 10; line++)
+	{
+		for (count = 0; count <= 14; count++)
+		{
+			num = count;
+			if (count > 9)
+			{
+				_putchar(1 + 48);
+				num = (count % 10);
+			}
+			_putchar(num + 48);
+		}
+		_putchar('\n');
+	}
+}
