57d77e7bd5e0ee77c7451826cba1e3302f6f0f12
G-omar-H <omarghazi12@gmail.com>
2023-06-22 11:01:58 -0700

    0x04C

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - multiply a and b
+ * @a: takes input from another function
+ * @b: takes input from another function
+ * Return: c product of a and b
+*/
+int mul(int a, int b)
+{
+	int c = a * b;
+
+	return (c);
+}
