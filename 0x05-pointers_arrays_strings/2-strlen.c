b588a78fa77d4b63424d4c34102e59d34abb82fc
G-omar-H <omarghazi12@gmail.com>
2023-06-27 16:26:02 -0700

    0x05c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,5 +1,9 @@
 #include "main.h"
-/***/
+/**
+ * _strlen - return lenght of a string
+ * @s: takes input from another function
+ * Return: lenght of string s
+ */
 int _strlen(char *s)
 {
 	return (strlen(s));
