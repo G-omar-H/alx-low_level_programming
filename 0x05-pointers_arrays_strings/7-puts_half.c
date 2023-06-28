f819a95ea43b96d5af3a4c05cb0120f1b3003734
G-omar-H <omarghazi12@gmail.com>
2023-06-28 09:17:28 -0700

    0x05c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,7 +9,10 @@ void puts_half(char *str)
 	int len, i, temp;
 
 	temp = strlen(str);
-	len = (temp) / 2;
+	if ((temp - 1) % 2 = 0)
+		len = (temp) / 2;
+	if else ((temp - 1) % != 0)
+		len = (temp - 1) / 2;
 	for (i = len ; i < temp; i++)
 	{
 		_putchar(str[i]);
