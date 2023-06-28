96854e0cfa1a6e0f062b40051de1e34e167a4000
G-omar-H <omarghazi12@gmail.com>
2023-06-28 08:34:35 -0700

    0x05c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * puts_half - print last half of a string
+ * @str: string
+ * Return: 0
+ */
+void puts_half(char *str)
+{
+	int len, i, temp;
+
+	temp = strlen(str);
+	len = (strlen(str) - 1) / 2;
+	for (i = len; i < temp; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
