223217f4fd91fc8291eb520113ae4b8ba46fd458
G-omar-H <omarghazi12@gmail.com>
2023-06-27 20:02:30 -0700

    0x05c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * puts2 - ff
+ * @str: out
+ * Return: 0
+*/
+void puts2(char *str)
+{
+	int i, len;
+
+	len = strlen(str)
+
+		for (i = 0; i < len; i++)
+		{
+			if (i % 2 != 0)
+				printf("%s\n", str);
+		}
+}
