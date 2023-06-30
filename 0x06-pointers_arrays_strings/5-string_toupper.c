470887b594e927b7da8cff7eebe864e0ea232c3a
G-omar-H <omarghazi12@gmail.com>
2023-06-30 12:01:23 -0700

    0x06c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -9,7 +9,7 @@ char *string_toupper(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] >= 97 && str[i] <= 122 )
+		if (str[i] >= 97 && str[i] <= 122)
 			str[i] = str[i] - 32;
 	}
 	return (str);
