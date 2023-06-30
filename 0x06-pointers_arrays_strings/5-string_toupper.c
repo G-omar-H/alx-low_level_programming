9ce8ff5ea6ed651d2c1f5b479fff81055c78e87e
G-omar-H <omarghazi12@gmail.com>
2023-06-30 11:58:35 -0700

    0x06c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * string_toupper - make a string uppercase
+ * Return: i
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 97 && str[i] <= 122 )
+			str[i] = str[i] - 32;
+	}
+	return (str);
+}
