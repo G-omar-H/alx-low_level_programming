643d289280f023d3f241d1b7cbfe473b7d737048
G-omar-H <omarghazi12@gmail.com>
2023-07-03 12:16:53 -0700

    0x07c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,12 +9,11 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int i = 0;
+	unsigned int i;
 
-	while (src[i] < src[n])
+	for (i = 0; i < n; i++)
 	{
-		dest = src;
-		i++;
+		dest[i] = src[i];
 	}
 	return (dest);
 }
