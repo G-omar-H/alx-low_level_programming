a7deccdffe8cc89d7f6261052c5d06a4867c7acf
G-omar-H <omarghazi12@gmail.com>
2023-07-03 12:33:14 -0700

    0x07c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -5,8 +5,18 @@
  * @s: string
  * @accept: string
  * Return: number of matching
-*/
+ */
 unsigned int _strspn(char *s, char *accept)
 {
-	return (strspn(s, accept));
+	unsigned int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != s[i]; j++)
+		{
+			if (accept[j] == '\0')
+				return (i);
+		}
+	}
+	return (i);
 }
