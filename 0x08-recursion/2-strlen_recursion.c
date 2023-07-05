d4eb8a9c29b68ec2e6d339eb80cb79a642d13821
G-omar-H <omarghazi12@gmail.com>
2023-07-05 12:44:23 -0700

    0x08c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strlen_recursion - print string's lenght
+ * @s: string
+ * Return: lenght
+ */
+int _strlen_recursion(char *s)
+{
+	int n = 0;
+	if (*s > '\0')
+		n += _strlen_recursion(s + 1) + 1;
+	return (n);
+}
