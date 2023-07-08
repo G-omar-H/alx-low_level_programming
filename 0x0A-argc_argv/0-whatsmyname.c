0e7af6fbfd2a8132f015218bdad51148794f3ac4
G-omar-H <omarghazi12@gmail.com>
2023-07-08 03:33:36 -0700

    0x0Ac

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - function that prints commands name
+ * @argc: counter
+ * @argv: vecter
+ * Return: 0
+ */
+int  main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
