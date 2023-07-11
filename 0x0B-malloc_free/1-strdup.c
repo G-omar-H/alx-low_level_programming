680c5d012431498d4ad0afd62a047255b0a77444
G-omar-H <omarghazi12@gmail.com>
2023-07-11 10:43:35 -0700

    0x0Bc

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -3,15 +3,16 @@
  * _strdup - duplicate a string into a new alocatted memory
  * @str: string to duplicate
  * Return: new pointer, or NULL if fail
-*/
-int _strlen(char *s);
+ */
 char *_strdup(char *str)
 {
 	char *ptr;
 	int i, len;
 
 	i = 0;
-	len = _strlen(str);
+	len = 0;
+	for (; str[len] != '\0'; len++)
+		;
 	if (str == NULL)
 		return (NULL);
 	ptr = malloc((len * sizeof(char)) + 1);
@@ -26,23 +27,6 @@ char *_strdup(char *str)
 			ptr[i] = str[i];
 			i++;
 		}
-		return (ptr);
 	}
-/**
- * _strlen - return lenght of a string
- * @s: takes input from another function
- * Return: lenght of string s
-*/
-int _strlen(char *s)
-{
-	int i, count;
-
-	count = 0;
-	i = 0;
-
-	do {
-		count += 1;
-		i++;
-	} while (s[i] != '\0');
-	return (count);
+	return (ptr);
 }
