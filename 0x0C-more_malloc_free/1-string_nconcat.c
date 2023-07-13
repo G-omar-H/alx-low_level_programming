ce29eab17c14b0c50d266e0c02194d9228b61e19
G-omar-H <omarghazi12@gmail.com>
2023-07-13 12:03:20 -0700

    0x0Cc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -3,10 +3,11 @@
  * strlen - to count string's elements
  * @s: string to count
  * Return: number of elements into the string
-*/
+ */
 int _strlen(char *s)
 {
 	int c;
+
 	for (c = 0; s[c] != '\0'; c++)
 		;
 	return (c);
@@ -17,35 +18,28 @@ int _strlen(char *s)
  * @s2: string to cocatinate
  * @n: number of elements to concatinate
  * Return: concatinated string
-*/
+ */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int i, j, nc;
-	unsigned int p, k;
+	char *str;
+	unsigned int i, j, nc;
 
 	nc = _strlen(s1) + n;
-	p = _strlen(s2);
-	s1 = malloc(nc * sizeof(char) + 1);
-	if (s1 == 0)
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	str = malloc(nc  * sizeof(char) + 1);
+	if (str == 0)
 		return (NULL);
 	for (i = 0; s1[i] != '\0'; i++)
-		;
-	for (j = 0; s2[j] != '\0'; j++)
+		str[i] = s1[i];
+	for (j = 0; j < n; j++)
 	{
-		if (n > p)
-			s1[i] = s2[j];
-		else
-		{
-			while (i < nc && k < n)
-			{
-				s1[i] = s2[k];
-			}
-			i++;
-			k++;
-			}
+		str[i] = s2[j];
+		i++;
 	}
-	i++;
-	s1[i] = '\0';
-	return (s1);
+	str[i] = '\0';
+	return (str);
 }
 
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -5,4 +5,5 @@
 #include <unistd.h>
 char _putchar(char c);
 void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
 #endif /*MAIN_H*/
