c94bf5aace8bcc3c425d3cbcd0223fa932514314
G-omar-H <omarghazi12@gmail.com>
2023-07-13 13:43:16 -0700

    0x0Cc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,5 +1,20 @@
 #include "main.h"
 /**
+ * _memset - fill a n number of memory block with a value
+ * @s: pointer
+ * @b: value to be filled
+ * @n:number of memory block
+ * Return: pointer to the memory filled
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *ptr = s;
+
+	while (n--)
+		*s++ = b;
+	return	(ptr);
+}
+/**
  * _calloc : allocate memory for an array, nd st memory to 0
  * @nmemb: number of elements in the array to be allocated
  * @size: size of each elem
@@ -11,8 +26,9 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	arr = calloc(nmemb,  sizeof(int));
+	arr = malloc(nmemb * size);
 	if (arr == 0)
 	       return (NULL);
+	_memset(arr, 0, size * nmemb);
 	return (arr);	
 }
