a00c65761537d93c09d146e72a0292881339c837
G-omar-H <omarghazi12@gmail.com>
2023-07-13 12:25:29 -0700

    0x0Cc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,18 +1,5 @@
 #include "main.h"
 /**
- * strlen - to count string's elements
- * @s: string to count
- * Return: number of elements into the string
- */
-int _strlen(char *s)
-{
-	int c;
-
-	for (c = 0; s[c] != '\0'; c++)
-		;
-	return (c);
-}
-/**
  * string_nconcat - concatinate n elements from a string
  * @s1: string to be concatinated
  * @s2: string to cocatinate
@@ -22,13 +9,17 @@ int _strlen(char *s)
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *str;
-	unsigned int i, j;
+	unsigned int i, j, _s1, _s2;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	str = malloc(_strlen(s1) + n + 1);
+	for (_s1 = 0; s1[_s1] != '\0'; _s1++)
+		;
+	for (_s2 = 0; s2[_s2] != '\0'; _s2++)
+		;
+	str = malloc(_s1 + n + 1);
 	if (str == 0)
 		return (NULL);
 	for (i = 0; s1[i] != '\0'; i++)
