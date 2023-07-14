30dabb5b4fd2e83fff2a426dcb1a2939209640d1
G-omar-H <omarghazi12@gmail.com>
2023-07-14 03:43:10 -0700

    0x0Cc

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -4,22 +4,18 @@
  * @min: first element on the array
  * @max: last element on the array
  * Return: poiter to the new array, null if fail
-*/
+ */
 int *array_range(int min, int max)
 {
 	int *arr;
-	int i;
-	long unsigned int j;
+	int i, j;
 
 	if (min > max)
 		return (NULL);
-	arr = malloc(sizeof(arr) * sizeof(int));
+	arr = malloc((max - min + 1) * sizeof(int));
 	if (arr == 0)
 		return (NULL);
-	for (i = min; i <= max; i++)
-	{
-		for (j = 0; j < sizeof(arr); j++)
-			arr[j] = i;
-	}
+	for (i = min, j = 0; i <= max; i++, j++)
+		arr[j] = i;
 	return (arr);
 }
