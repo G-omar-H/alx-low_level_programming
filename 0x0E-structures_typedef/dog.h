b3eb5215b0d45bf2ccf7286d58a27d97fdba363c
G-omar-H <omarghazi12@gmail.com>
2023-07-17 15:43:00 -0700

    0x0E C

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - short def
+ * @name: dogname
+ * @age: dog age
+ * @owner: owner name
+ * Description: POPPY
+*/
 typedef struct dog
 {
 	char *name;
