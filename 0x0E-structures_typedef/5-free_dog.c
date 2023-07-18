6c9143d65fff7f8307c9b8ad0f50bd5649689c44
G-omar-H <omarghazi12@gmail.com>
2023-07-18 16:08:26 -0700

    0x0E c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - frees you're dog
+ * @d: you're dog
+ * Return: void.
+ */
+void free_dog(dog_t *d)
+{
+	if (d->name)
+		free(d->name);
+	if (d->owner)
+		free(d->owner);
+	free(d);
+}
