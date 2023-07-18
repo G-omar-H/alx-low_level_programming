254338d3579ee777e9f1e7356da91c38eda71274
G-omar-H <omarghazi12@gmail.com>
2023-07-18 14:41:23 -0700

    0x0E c

--- 0x0E-structures_typedef/2-main.c
+++ 0x0E-structures_typedef/2-main.c
@@ -11,7 +11,8 @@ int main(void)
 	struct dog my_dog;
 
 	my_dog.name = NULL;
-	my_dog.owner = "Bob";
+	my_dog.age = 3.5;
+	my_dog.owner = '\0';
 	print_dog(&my_dog);
 	return (0);
 }
--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -4,24 +4,13 @@
  * print_dog - print a struct elements
  * @d: dog's init
  * Return: void.
-*/
+ */
 void print_dog(struct dog *d)
 {
 	if (d)
 	{
-		if (d->name == NULL)
-		{
-			printf("name: (nil)\nAge: %f\nOwner: %s\n", d->age, d->owner);
-		}
-		else if (!(d->age))
-		{
-			printf("name: %s\nAge:(nil)\nOwner: %s\n", d->name, d->owner);
-		}
-		else if (d->owner == NULL)
-		{
-			printf("name: %s\nAge: %f\nOwner: (nil)\n", d->name, d->age);
-		}
-		else
-			printf("name: %s\nAge: %f\nOwner: %s\n",d->name, d->age, d->owner);
+		printf("name: %s\n", d->name ? d->name : "(nil)");
+		printf("Age: %f\n", d->age);
+		printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
 	}
 }
