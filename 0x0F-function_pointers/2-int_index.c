e716541f19750376c982d6eec044b7da7e7cc3d7
G-omar-H <omarghazi12@gmail.com>
2023-07-19 12:39:08 -0700

    0x0E c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,7 +12,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (size <= 0)
 		return (-1);
-	if (array || size || cmp)
+	if (array && size && cmp)
 	{
 		for (i = 0; i < size; i++)
 		{
@@ -20,8 +20,6 @@ int int_index(int *array, int size, int (*cmp)(int))
 			if (cmp(array[i]) == 1)
 				return (i);
 		}
-		if (cmp(array[i]) == 0)
-			return (-1);
 	}
-	return (0);
+	return (-1);
 }
