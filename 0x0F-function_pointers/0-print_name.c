e12946dec0f795c13b01152a33226d5ff9c1e200
G-omar-H <omarghazi12@gmail.com>
2023-07-19 08:30:58 -0700

    0x0F c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+/**
+ * print_name - function that call back a function usinf function's pointer
+ * @name: name to print
+ * @f: function pointer
+ * Return: void.
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL)
+		f(name);
+}
