9b950611ea79a5d52befa443ab040f73f34e543f
G-omar-H <omarghazi12@gmail.com>
2023-07-20 04:21:46 -0700

    0x0varf c

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+/**
+ * print_numbers - function to print variadicc args seperated by a strin
+ * @separator: string to seperat arguments
+ * @n: first required arg
+ * Return: void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i = 0;
+
+	va_start(ap, n);
+	while (i < n)
+	{
+		printf("%d", va_arg(ap, int));
+		if (separator && i < n - 1)
+		{
+			printf("%s ", separator);
+		}
+		i++;
+	}
+	printf("\n");
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,4 +6,5 @@
 #include <string.h>
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
 #endif /*VARIADIC_FUNCTIONS_H*/
