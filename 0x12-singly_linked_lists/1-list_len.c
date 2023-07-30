a599940b6dd43dab9acdf6197c5f34b3702f5932
G-omar-H <omarghazi12@gmail.com>
2023-07-30 10:26:47 -0700

    0x12C

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * list_len - count number of elements in a linked list
+ * @h: pointer to an element
+ * Return: number of elements
+*/
+size_t list_len(const list_t *h)
+{
+	size_t n = 0;
+
+	while (h != NULL)
+	{
+		n++;
+		h = h->next;
+	}
+	return (n);
+}
