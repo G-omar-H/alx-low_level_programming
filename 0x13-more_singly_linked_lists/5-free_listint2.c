cf8a66b82088761c66100c95317884571a389dab
G-omar-H <omarghazi12@gmail.com>
2023-07-31 13:37:27 -0700

    0x13C

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,7 +9,7 @@ void free_listint2(listint_t **head)
 	listint_t *current = *head;
 
 	if (head == NULL || *head == NULL)
-		return ;
+		return;
 	while (current != NULL)
 	{
 		(*head) = current->next;
