4a6743b2f6256b67a8001688588b4a2cec345c69
G-omar-H <omarghazi12@gmail.com>
2023-08-01 01:43:12 -0700

    0x13C

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,6 +16,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	new->n = n;
 	new->next = NULL;
+	if (idx == 0)
+	{
+		new->next = previous;
+		*head = new;
+		return (new);
+	}
 	while (idx > 1 && previous != NULL)
 	{
 		idx--;
