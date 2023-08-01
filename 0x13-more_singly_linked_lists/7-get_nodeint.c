77fe0155716022fd6883676cb2e7fc8d44e0f2fa
G-omar-H <omarghazi12@gmail.com>
2023-08-01 00:29:40 -0700

    0x13C

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,16 +1,27 @@
 #include "lists.h"
-/***/
+/**
+ * get_nodeint_at_index - set to the nth node
+ * @head: pointer to the first node
+ * @index: the node number
+ * Return: pointer to the nth node, NULL if it's not existing
+ */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	listint_t *current = head;
+	unsigned int nodes = 1;
 
+	while (current != NULL)
+	{
+		nodes++;
+		current = current->next;
+	}
+	if (nodes < index)
+	{
+		printf("Error");
+		return (NULL);
+	}
 	while (index > 0 && current != NULL)
 	{
-		if (current->next == NULL)
-		{
-			printf("Error");
-			return (NULL);
-		}
 		index--;
 		current = current->next;
 	}
