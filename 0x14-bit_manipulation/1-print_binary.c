75e106974ad477a69f177f11bb4be805a0e6a6bf
G-omar-H <omarghazi12@gmail.com>
2023-08-04 04:25:01 -0700

    0x14C

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -3,17 +3,15 @@
  * print_binary - find bianry representation of a number
  * @n: takes inpute from another function
  * Return: void.
-*/
+ */
 void print_binary(unsigned long int n)
 {
-	unsigned long binary = 0, i = 1;
-
-	while (n > 0)
+	if (n <= 1)
 	{
-		if ((n & 1) == 1)
-			binary += i;
-		i *= 10;
-		n >>= 1;
+		_putchar(n + '0');
+		return;
 	}
-	printf("%lu", binary);
+	print_binary(n >> 1);
+	_putchar((n & 1) ?  '1' : '0');
 }
+
