55f383e921ff7a117d0fbcf9f20e5a4ee14215df
G-omar-H <omarghazi12@gmail.com>
2023-08-04 13:52:20 -0700

    0x14C

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -7,17 +7,7 @@
 */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long set = 1;
-
-	while (index > 0)
-	{
-		if (set > UINT_MAX)
-			return (-1);
-		set <<= 1;
-		index--;
-	}
-	if (index != 0)
+	if (index >= sizeof(n) * 8)
 		return (-1);
-	*n = *n | set;
-	return (1);
+	return (*n |= 1UL << index);
 }
