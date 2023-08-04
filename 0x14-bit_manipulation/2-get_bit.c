15d3fcb0e534e4f8c30c5d46e084638d8b933869
G-omar-H <omarghazi12@gmail.com>
2023-08-04 13:42:54 -0700

    0x14C

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -4,17 +4,10 @@
  * @n: takes input from another function
  * @index: nth bit
  * Return: bit at index, -1 if fails
-*/
+ */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned int count = index;
-
-	while (count > 0 && n)
-	{
-		n >>= 1;
-		count--;
-	}
-	if (count != 0)
+	if (idex >= sizeof(n) * 8)
 		return (-1);
-	return ((n & 1) ? 1 : 0);
+	return ((n >> index) & 1);
 }
