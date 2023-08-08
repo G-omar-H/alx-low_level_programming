01abb166d624ddd4c703880cfa673809598fd9db
G-omar-H <omarghazi12@gmail.com>
2023-08-08 01:47:10 -0700

    0x15C

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * append_text_to-file - concatinate a text into a file
+ * append_text_to_file - concatinate a text into a file
  * @filename: pointer to filename
  * @text_content: text to concatinate
  * Return: 1 at success, -1 if fail
 */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, count, len = 0;
+	int fd, counti = 0, len = 0;
 
 	if (!filename)
 		return (-1);
