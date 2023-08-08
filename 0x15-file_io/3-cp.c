6dcbea3d888ba483f8ff0739d1a98ec65322e40a
G-omar-H <omarghazi12@gmail.com>
2023-08-08 11:33:25 -0700

    0x15C

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -21,7 +21,7 @@ void close_file(int fd)
 int main(int ac, char **av)
 {
 	int fd1, fd2;
-	ssize_t rd;
+	ssize_t R, W;
 	char *buff;
 
 	if (ac != 3)
@@ -30,28 +30,32 @@ int main(int ac, char **av)
 		exit(97);
 	}
 	buff = malloc(sizeof(char) * 1024);
-        if (buff ==  NULL)
-        {
-                dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-                exit(99);
-        }
+	if (buff ==  NULL)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		exit(99);
+	}
 	fd1 = open(av[1], O_RDONLY);
+	R = read(fd1, buff, 1024);
 	fd2 = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR
 			| S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-	while ((rd = read(fd1, buff, 1024) > 0))
+	while ((R > 0))
 	{
-		if (rd == -1 || fd1 == -1)
+		if (R == -1 || fd1 == -1)
 		{
 			free(buff);
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 			exit(98);
 		}
-		if (write(fd2, buff, rd) != rd || fd2 == -1)
+		W = write(fd2, buff, R);
+		if (fd2 == -1 || W == -1 || W != R)
 		{
 			free(buff);
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 			exit(99);
 		}
+		R = read(fd1, buff, 2024);
+		fd2 = open(av[2], O_WRONLY, O_APPEND);
 	}
 	free(buff);
 	close_file(fd1);
