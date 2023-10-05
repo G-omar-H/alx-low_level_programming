305be7cf10a05dff761efd4c9635d63bed5c4ecb
G-omar-H <omarghazi12@gmail.com>
2023-10-05 23:09:48 +0100

    hash table items getter implemented!

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -7,9 +7,13 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	unsigned long index = key_index((const unsigned char *)key, ht->size);
-	hash_node_t *item = ht->array[index];
+	unsigned long index;
+	hash_node_t *item;
 
+	if (ht == NULL)
+		return (NULL);
+	index = key_index((const unsigned char *)key, ht->size);
+	item = ht->array[index];
 	if (item != NULL)
 	{
 		if (strcmp(item->key, key) == 0)
