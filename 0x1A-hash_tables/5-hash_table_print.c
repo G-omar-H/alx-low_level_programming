968a5254f3f211a7ad9bf142c05c83be72d148e9
G-omar-H <omarghazi12@gmail.com>
2023-10-06 10:14:45 +0100

    hash table printer implemeted!

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -10,49 +10,46 @@ void hash_table_print(const hash_table_t *ht)
 	unsigned long int i, count = 0, index = 0;
 	hash_node_t *item;
 
-	if (ht == NULL)
-		printf("{}\n'");
-	else
+	if (ht == NULL || ht->array == NULL)
+		return;
+	printf("{");
+	for (i = 0; i < ht->size; i++)
 	{
-		printf("{");
-		for (i = 0; i < ht->size; i++)
+		if (ht->array[i])
 		{
-			if (ht->array[i])
+			item = ht->array[i];
+			if (ht->array[i]->next)
 			{
-				item = ht->array[i];
-				if (ht->array[i]->next)
+				while (item->next)
 				{
-					while (item->next)
-					{
-						item = item->next;
-						count++;
-					}
+					item = item->next;
+					count++;
 				}
-				count++;
 			}
+			count++;
 		}
-		for (i = 0; i < ht->size; i++)
-		{
-			if (ht->array[i])
-			{
-				item = ht->array[i];
-				printf("'%s': '%s'", item->key, item->value);
-				if (index < (count - 1))
-				{
-					printf(", ");
-				}
-				if (ht->array[i]->next)
-				{
-					while (item->next)
-					{
-						item = item->next;
-						printf("'%s': '%s'", item->key, item->value);
-						index++;
-					}
-				}
-				index++;
-			}
-		}
-		printf("}\n");
 	}
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i])
+		{
+			item = ht->array[i];
+			printf("'%s': '%s'", item->key, item->value);
+			if (index < (count - 1))
+			{
+				printf(", ");
+			}
+			if (ht->array[i]->next)
+			{
+				while (item->next)
+				{
+					item = item->next;
+					printf("'%s': '%s'", item->key, item->value);
+					index++;
+				}
+			}
+			index++;
+		}
+	}
+	printf("}\n");
 }
