120268ccc376895091265137f887ffe1d42bdad5
G-omar-H <omarghazi12@gmail.com>
2023-10-06 08:36:39 +0100

    hash table getter implemeted!

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -8,16 +8,14 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long index;
-	hash_node_t *item;
 
 	if (ht == NULL || key == NULL)
 		return (NULL);
 	index = key_index((const unsigned char *)key, ht->size);
-	item = ht->array[index];
-	if (item != NULL)
+	if (ht->array[index])
 	{
-		if (strcmp(item->key, key) == 0)
-			return (item->value);
+		if (strcmp(ht->array[index]->key, key) == 0)
+			return (ht->array[index]->value);
 	}
 	return (NULL);
 }
