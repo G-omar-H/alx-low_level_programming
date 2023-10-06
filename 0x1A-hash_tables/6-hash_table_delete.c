77f6ffe71c6e1113ce9bc66ec3be57a31d2c45d8
G-omar-H <omarghazi12@gmail.com>
2023-10-06 23:31:31 +0100

    hash tables implemented sucessfully~

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,25 +1,29 @@
 #include "hash_tables.h"
-/***/
+/**
+ * hash_table_delete - delelte a hash table
+ * @ht: hash table to compute
+ * Return: void.
+ */
 void hash_table_delete(hash_table_t *ht)
 {
-    unsigned long int i = 0;
-    hash_node_t *curr;
+	unsigned long int i = 0;
+	hash_node_t *curr;
 
-    if (!ht || !ht->array || ht->size == 0)
-        return;
-    for (i = 0; i < ht->size; i++)
-    {
-        while (ht->array[i])
-        {
-            curr = ht->array[i]->next;
-            free(ht->array[i]->key);
-            free(ht->array[i]->value);
-            free(ht->array[i]);
-            ht->array[i] = curr;
-        }
-    }
-    free(ht->array);
-    ht->array = NULL;
-    ht->size = 0;
-    free(ht);
+	if (!ht || !ht->array || ht->size == 0)
+		return;
+	for (i = 0; i < ht->size; i++)
+	{
+		while (ht->array[i])
+		{
+			curr = ht->array[i]->next;
+			free(ht->array[i]->key);
+			free(ht->array[i]->value);
+			free(ht->array[i]);
+			ht->array[i] = curr;
+		}
+	}
+	free(ht->array);
+	ht->array = NULL;
+	ht->size = 0;
+	free(ht);
 }
