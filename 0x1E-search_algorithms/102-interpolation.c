74f2c5d63590550f76e919e12851f86cf4043073
G-omar-H <omarghazi12@gmail.com>
2024-01-17 12:59:25 +0100

    9. Interpolation search

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -30,7 +30,7 @@ int interpolation_search(int *array, size_t size, int value)
 				break;
 			}
 			else
-				printf(" = %d\n", array[pos]);
+				printf(" = [%d]\n", array[pos]);
 
 			if (array[pos] == value)
 				return (pos);
