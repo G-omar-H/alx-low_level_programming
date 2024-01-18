8611884576fc21cb98f22390cba06b159f2fe087
G-omar-H <omarghazi12@gmail.com>
2024-01-18 12:58:36 +0100

    11. Advanced binary search #advanced

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -1,44 +1,58 @@
 #include "search_algos.h"
 /**
- * advanced_binary - looks for the first occurence of an  element
- * on a sorted array
+ * rec_search - looks for the first occurence of an  element
+ * on a sorted array recursively
  * @array: pointer to the first element of a list
- * @f: first index
  * @size: size of the array
  * @value: value to search for
  * Return: index of the element where value, -1 if unseccessful
  */
-int advanced_binary(int *array, size_t f, size_t size, int value)
+int rec_search(int *array, size_t size, int value)
 {
-	size_t i = 0, l, m;
+	size_t  m, i;
 
-	l = size - 1;
-	m = (f + l) / 2;
+
+	m = size / 2;
 
 	if (array && size > 0)
 	{
 		printf("Searching in array: ");
-		for (i = f; i <= l; i++)
-		{
-			if (i == size - 1)
-			{
-				printf("%d\n", array[i]);
-				break;
-			}
-			printf("%d, ", array[i]);
-		}
+		for (i = 0; i < size; i++)
+			printf("%s %d", (i == 0) ? ":" : ",", array[i]);
+		printf("\n");
+		if (m && size % 2 == 0)
+			m--;
 		if (array[m] == value)
 		{
-			if (array[m - 1] == value)
-				return (advanced_binary(array, f, m, value));
-			return (m);
+			if (m > 0)
+				return (advanced_binary(array, m + 1, value));
+			return ((int)m);
 		}
-		else if (m  == l && array[m] != value)
-			return (-1);
-		else if (array[m] < value)
-			return (advanced_binary(array, m + 1, size, value));
-		else
-			return (advanced_binary(array, f, m, value));
+
+		if (array[m] > value)
+			return (advanced_binary(array, m + 1, value));
+		m++;
+		return (advanced_binary(array + m, size - m, value) + m);
 	}
 	return (-1);
+}
+/**
+ * advanced_binary - calls rec_search to retur the idex of an element
+ * on a sorted array recursively
+ * @array: pointer to the first element of a list
+ * @size: size of the array
+ * @value: value to search for
+ * Return: index of the element where value, -1 if unseccessful
+ */
+
+int advanced_binary(int *array, size_t size, int value)
+{
+	int index;
+
+	index = rec_search(array, size, value);
+
+	if (index >= 0 && array[index] != value)
+		return (-1);
+
+	return (index);
 }
