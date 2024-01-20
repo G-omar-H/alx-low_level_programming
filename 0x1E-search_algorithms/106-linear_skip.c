881069703f5fc180e78812e554dab7e53273d1e6
G-omar-H <omarghazi12@gmail.com>
2024-01-20 22:07:53 +0100

    13. Linear search in a skip list

--- 0x1E-search_algorithms/106-linear_skip.c
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -1,70 +1,45 @@
 #include "search_algos.h"
-skiplist_t *linear_skip(skiplist_t *list, int value);
+
 /**
- * linear_skip - search for an element on a sorted skip list
- * @list: pointer to the first node of the lincked list
+ * linear_skip - searches for a value in a skip list
+ *
+ * @list: input list
  * @value: value to search in
- * Return: pointer to the element
+ * Return: index of the number
  */
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-	skiplist_t *node, *sub, *prev;
-	size_t save, i;
+	skiplist_t *go;
 
-	node = sub = prev = list;
 	if (list == NULL)
 		return (NULL);
 
-	while (node)
+	go = list;
+
+	do {
+		list = go;
+		go = go->express;
+		printf("Value checked at index ");
+		printf("[%d] = [%d]\n", (int)go->index, go->n);
+	} while (go->express && go->n < value);
+
+	if (go->express == NULL)
 	{
-		save = prev->index;
-		prev = node;
-		node = node->express;
-		if (node)
-			printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
-
-		for (i = 0; i < prev->index - save; i++)
-		{
-			if (node)
-				sub = sub->next;
-
-			if (!node)
-			{
-				while (sub->index < prev->index)
-					sub = sub->next;
-				prev = sub;
-				while (sub->next)
-					sub = sub->next;
-				break;
-			}
-
-		}
-		if (node)
-		{
-			if (node->n > value)
-			{
-				printf("Value found between indexes [%ld] and [%ld]\n",
-						prev->index, node->index);
-				while (sub->index <= node->index)
-				{
-					printf("Value checked at index [%ld] = [%d]\n", sub->index, sub->n);
-					if (sub->n == value)
-						return (sub);
-					sub = sub->next;
-				}
-				return (NULL);
-			}
-		}
+		list = go;
+		while (go->next)
+			go = go->next;
 	}
-	printf("Value found between indexes [%ld] and [%ld]\n",
-			prev->index, sub->index);
 
-	while (prev)
+	printf("Value found between indexes ");
+	printf("[%d] and [%d]\n", (int)list->index, (int)go->index);
+
+	while (list != go->next)
 	{
-		printf("Value checked at index [%ld] = [%d]\n", prev->index, prev->n);
-		if (prev->n == value)
-			return (prev);
-		prev = prev->next;
+		printf("Value checked at index [%d] = [%d]\n", (int)list->index, list->n);
+		if (list->n == value)
+			return (list);
+		list = list->next;
 	}
+
 	return (NULL);
 }
